551a15f49d8cf5c793e357d40bfc4389e04fc735
David Hunt <huntdog1541@gmail.com>
2019-12-14 01:04:33 -0600

    Initial commit

---
+++ src/main/java/com/keystonetest1/vscoderemote/VscodeRemoteApplication.java
@@ -0,0 +1,13 @@
+package com.keystonetest1.vscoderemote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VscodeRemoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VscodeRemoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keystonetest1/vscoderemote/VscodeRemoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keystonetest1.vscoderemote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VscodeRemoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
